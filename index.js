3e06683166307b83eff5b566b3a98fd3c36499f4
Sliferyfire <sliferyfire@gmail.com>
2024-10-24 17:13:56 -0600

    third commit

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var app = express();
 var rutas = require("./routes/rutas");
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use("/", rutas);
+
 
 var port = process.env.PORT || 8000;
 app.listen(port,()=>{
--- routes/rutas.js
+++ routes/rutas.js
@@ -8,4 +8,21 @@ ruta.get("/inicio",(req,res) =>{
     res.render("inicio");
 })
 
+ruta.post("/iniciarSesion", (req,res)=>{
+    // console.log("Datos a enviar: " + req.body.email + " " + req.body.password);
+    fetch('http://localhost:9001/auth/autenticacion', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(req.body)
+    })
+    .then(response => response.json())
+    .then(respuesta => {
+        console.log('Inicio de sesion exitoso:', respuesta);
+        res.redirect("/inicio");
+    })
+    .catch(error => {
+        console.error('Error al iniciar sesion:', error);
+    });
+})
+
 module.exports = ruta;
