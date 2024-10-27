24a2180e0539de2c9fa882c81a32f06b64e80b45
Sliferyfire <sliferyfire@gmail.com>
2024-10-27 14:28:27 -0600

    prueba vercel 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var rutas = require("./routes/rutas");
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
