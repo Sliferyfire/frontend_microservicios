d0cc99c26d86cc4fddcecaa6ca94230e73b13770
Sliferyfire <sliferyfire@gmail.com>
2024-10-28 00:57:53 -0600

    subida a vercel

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var app = express();
 var rutas = require("./routes/rutas");
-const path = require('path');
+var path = require('path');
+const cookieParser = require('cookie-parser');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", rutas);
-
 
 var port = process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log("Servidor en http://localhost:"+port);
 });
+
--- routes/rutas.js
+++ routes/rutas.js
@@ -5,16 +5,25 @@ ruta.get("/", (req,res)=>{
 })
 
 ruta.post("/iniciarSesion", (req,res)=>{
-    // console.log("Datos a enviar: " + req.body.email + " " + req.body.password);
+    console.log("Datos a enviar: " + req.body.email + " " + req.body.password);
     fetch('https://api-gateway-indol.vercel.app/auth/authentication/signIn', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body)
     })
     .then(response => response.json())
-    .then(respuesta => {
-        console.log('Inicio de sesion exitoso:', respuesta);
-        res.redirect("/inicio");
+    .then(data => {
+        if (data.token) {
+            res.cookie('authToken', data.token, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 24 * 60 * 60 * 1000
+            });
+            console.log('Inicio exitoso:', data);
+            res.redirect("/inicio");
+        } else {
+            throw new Error('No se recibió un token');
+        }
     })
     .catch(error => {
         console.error('Error al iniciar sesion:', error);
@@ -34,9 +43,18 @@ ruta.post("/registrarCuenta", (req,res)=>{
         body: JSON.stringify(req.body)
     })
     .then(response => response.json())
-    .then(respuesta => {
-        console.log('Registro exitoso:', respuesta);
-        res.redirect("/inicio");
+    .then(data => {
+        if (data.token) {
+            res.cookie('authToken', data.token, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 24 * 60 * 60 * 1000
+            });
+            console.log('Registro exitoso:', data);
+            res.redirect("/inicio");
+        } else {
+            throw new Error('No se recibió un token');
+        }
     })
     .catch(error => {
         console.error('Error al registrar cuenta:', error);
@@ -45,94 +63,125 @@ ruta.post("/registrarCuenta", (req,res)=>{
 }) 
 
 ruta.get("/inicio", (req,res) =>{
-    fetch('https://api-gateway-indol.vercel.app/agendar/mostrarCitas', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(response => response.json())
-    .then(citas => {
-        console.log('Citas: ', citas);
-        res.render("inicio",{citas});
-    })
-    .catch(error => {
-        console.error('Error al recuperar datos:', error);
-        res.render("inicio", { citas: [] });
-    });
+    const token = req.cookies.authToken;
+    if (token) {
+        fetch('https://api-gateway-indol.vercel.app/agendar/mostrarCitas', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(citas => {
+            console.log('Citas: ', citas);
+            res.render("inicio",{citas});
+        })
+        .catch(error => {
+            console.error('Error al recuperar datos:', error);
+            res.render("inicio", { citas: [] });
+        });
+    } else {
+        res.redirect("/");
+    }
 })
 
 ruta.get("/agendar", (req,res)=>{
-    res.render("agendar");
+    const token = req.cookies.authToken;
+    if (token) {
+        res.render("agendar");
+    } else {
+        res.redirect("/");
+    }
 })
 
 ruta.post("/agendarCita", (req,res)=>{
-    // console.log(req.body);
-    fetch('https://api-gateway-indol.vercel.app/agendar/nuevacita', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(req.body)
-    })
-    .then(response => response.json())
-    .then(respuesta => {
-        console.log('Agenda de cita exitoso: ', respuesta);
-        res.redirect("/inicio");
-    })
-    .catch(error => {
-        console.error('Error al agendar la cita:', error);
-        res.redirect("/agendar");
-    });
+    const token = req.cookies.authToken;
+    if (token) {
+        console.log(req.body);
+        fetch('https://api-gateway-indol.vercel.app/agendar/nuevacita', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(req.body)
+        })
+        .then(response => response.json())
+        .then(respuesta => {
+            console.log('Agenda de cita exitoso: ', respuesta);
+            res.redirect("/inicio");
+        })
+        .catch(error => {
+            console.error('Error al agendar la cita:', error);
+            res.redirect("/agendar");
+        });
+    } else {
+        res.redirect("/");
+    }
 })
 
 ruta.get("/editar/:id", (req,res)=>{
-    fetch(`https://api-gateway-indol.vercel.app/agendar/buscarPorID/${req.params.id}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(response => response.json())
-    .then(cita => {
-        console.log('Cita: ', cita);
-        res.render("editar", {cita});
-    })
-    .catch(error => {
-        console.error('Error al recuperar cita:', error);
-        res.redirect("/inicio");
-    });
+    const token = req.cookies.authToken;
+    if (token) {
+        fetch(`https://api-gateway-indol.vercel.app/agendar/buscarPorID/${req.params.id}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(cita => {
+            console.log('Cita: ', cita);
+            res.render("editar", {cita});
+        })
+        .catch(error => {
+            console.error('Error al recuperar cita:', error);
+            res.redirect("/inicio");
+        });
+    } else {
+        res.redirect("/");
+    }
 })
 
 ruta.post("/editar", (req,res)=>{
-    fetch(`https://api-gateway-indol.vercel.app/agendar/editar`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(req.body)
-    })
-    .then(response => response.json())
-    .then(respuesta => {
-        console.log('Cita modificada: ', respuesta);
-        res.redirect("/inicio");
-    })
-    .catch(error => {
-        console.error('Error al modificar la cita:', error);
-        res.redirect("/inicio");
-    });
+    const token = req.cookies.authToken;
+    if (token) {
+        fetch(`https://api-gateway-indol.vercel.app/agendar/editar`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(req.body)
+        })
+        .then(response => response.json())
+        .then(respuesta => {
+            console.log('Cita modificada: ', respuesta);
+            res.redirect("/inicio");
+        })
+        .catch(error => {
+            console.error('Error al modificar la cita:', error);
+            res.redirect("/inicio");
+        });
+    } else {
+        res.redirect("/");
+    }   
 })
 
 ruta.get("/borrar/:id", (req,res)=>{
-    fetch(`https://api-gateway-indol.vercel.app/agendar/borrar/${req.params.id}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(response => response.json())
-    .then(cita => {
-        console.log('Cita borrada: ', cita);
-        res.redirect("/inicio");
-    })
-    .catch(error => {
-        console.error('Error al borrar cita:', error);
-        res.redirect("/inicio");
-    });
+    const token = req.cookies.authToken;
+    if (token) {
+        fetch(`https://api-gateway-indol.vercel.app/agendar/borrar/${req.params.id}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(cita => {
+            console.log('Cita borrada: ', cita);
+            res.redirect("/inicio");
+        })
+        .catch(error => {
+            console.error('Error al borrar cita:', error);
+            res.redirect("/inicio");
+        });
+    } else {
+        res.redirect("/");
+    }  
 })
 
-ruta.get("/cerrarSesion", (req,res)=>{
-    res.redirect("/");
-})
+ruta.get("/cerrarSesion", (req, res) => {
+    res.clearCookie('authToken');
+    res.redirect("/"); 
+});
 
 module.exports = ruta;
